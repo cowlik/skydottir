d05d38b1d2cb9ec73a2e899ad57fb2c18279c1f3
cowlik <clint@cowlik.com>
2020-11-20 18:25:19 -0800

    added header logo

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,7 +1,16 @@
 import styles from './Header.module.scss';
 
 const Header = () => {
-	return <header className={styles.container}>Header</header>;
+	return (
+		<header className={styles.container}>
+			<img
+				src='./assets/images/logo.svg'
+				width='405'
+				height='307'
+				alt='Skydottir Productions'
+			/>
+		</header>
+	);
 };
 
 export default Header;
